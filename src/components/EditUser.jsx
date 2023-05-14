bc596138b66343a9987e31ee35ed82d77ae4011b
Bijen Rayamajhi <bijenrayamajhi@gmail.com>
2023-05-14 17:47:14 +0545

    User control added

---
+++ src/components/EditUser.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "../stylesheets/editUser.css";
+const EditUser = () => {
+  const data = [
+    { id: 1, name: "John", age: 25 },
+    { id: 2, name: "Jane", age: 30 },
+    { id: 3, name: "Bob", age: 35 },
+    { id: 4, name: "Alice", age: 28 },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState(data);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    const filteredResults = data.filter((item) =>
+      item.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setSearchResults(filteredResults);
+  };
+
+  const handleDelete = (id) => {
+    const updatedResults = searchResults.filter((item) => item.id !== id);
+    setSearchResults(updatedResults);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {searchResults.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+              <td>
+                <button
+                  className="button"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default EditUser;
--- src/pages/UserControl.jsx
+++ src/pages/UserControl.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import UserTabs from "../components/UserTabs";
 
 const UserControl = () => {
     return (
         <div>
             <h1>User Control page</h1>
+            <UserTabs/>
         </div>
     );
 };
